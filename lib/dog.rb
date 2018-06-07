07b9cbf00287bc1686b34024b10b343ad7fb5e6e
sirikops <sirikops@hotmail.com>
2018-06-07 22:05:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # dog.rb
+class Dog
+  def initialize(fido, snoopy, lassie)
+    @fido=fido
+    @snoopy=snoopy
+    @lassie=lassie
+    
+    def fido
+      @fido=fido
+    end
+    def fido=(fido)
+      @fido=fido
+    end
+    
+    def snoopy
+      @snoopy=snoopy
+    end
+    def snoopy=(snoopy)
+      @snoopy=snoopy
+    end
+    
+    def lassie
+      @lassie=lassie
+    end
+    def lassie=(lassie)
+      @lassie=lassie
+    end
+  
+  end
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+class Person
+def initialize(adele_goldberg, alan_kay)
+  @adele_goldberg=adele_goldberg
+  @alan_kay=alan_kay
+  
+  def adele_goldberg
+    @adele_goldberg=adele_goldberg
+  end
+  def adele_goldberg=(adele_goldberg)
+  @adele_goldberg=adele_goldberg
+end
+
+  def alan_kay
+    @alan_kay=alan_kay
+  end
+  def alan_kay=(alan_kay)
+    @alan_kay=alan_kay
+  end
+  
+end
