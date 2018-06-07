3f8700342009d545e4efa5663186b46858aec581
sirikops <sirikops@hotmail.com>
2018-06-07 22:03:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,3 +3,18 @@ class Person
 def initialize(adele_goldberg, alan_kay)
   @adele_goldberg=adele_goldberg
   @alan_kay=alan_kay
+  
+  def adele_goldberg
+    @adele_goldberg=adele_goldberg
+  end
+  def adele_goldberg=(adele_goldberg)
+  @adele_goldberg=adele_goldberg
+end
+
+  def alan_kay
+    @alan_kay=alan_kay
+  end
+  def alan_kay=(alan_kay)
+    @alan_kay=alan_kay
+  end
+end
